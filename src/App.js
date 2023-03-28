71c433e0680ea0762a0a9ea69c2d4b8a3923c99a
Dheeraj-parashar <127424719+Dheeraj-parashar@users.noreply.github.com>
2023-03-28 21:54:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import React from 'react';
 import Element from './components/element';
 
 function App() {
+  const [value,setValue]=useState(null)
+  const[eValue,seteValue]=useSate([])
+  function handlePush(e){
+    setValue(e.target.value)
+    seteValue(...eValue.push(value))
+    return <Element value={value}/>   
+  }
+  function handlePop(){
+    if(eValue==null) window.alert("stack is empty")
+    else{
+      seteValue(...eValue.pop())
+    return <Element value={value}/>   
+    }
+  }
+  function handleTop(){
+    if(eValue==null) window.alert("stack is empty")
+    else{
+      
+    }
+  }
+
   return (
     <div className="container">
       <h1 className="header">Stack</h1>
@@ -14,19 +35,19 @@ function App() {
         <div className="col md-3 operation">
           <h2>Operations</h2>
           <div className="input-group push">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={(e)=>{handlePush(e)}}>
               PUSH
             </button>
             <input type="text" className="form-control" />
           </div>
           <div className="input-group pop">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={()=>{handlePop}}>
               POP
             </button>
             <input type="text" disabled className="form-control" />
           </div>
           <div className="input-group top">
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={()=>{handleTop}}>
               TOP
             </button>
             <input type="text" disabled className="form-control" />
